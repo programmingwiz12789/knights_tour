5a552a8d14b36f99f8540f08569621280731ac32
Programming Wiz <allisprogramming12789@gmail.com>
2026-03-18 03:13:39 +0700

    Initial commit

---
+++ Knight's Tour/Knight's Tour/Form1.Designer.vb
@@ -0,0 +1,494 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class KnightsTour
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        cell00 = New Button()
+        cell01 = New Button()
+        cell02 = New Button()
+        cell03 = New Button()
+        cell04 = New Button()
+        cell05 = New Button()
+        cell11 = New Button()
+        cell10 = New Button()
+        cell09 = New Button()
+        cell08 = New Button()
+        cell07 = New Button()
+        cell06 = New Button()
+        cell17 = New Button()
+        cell16 = New Button()
+        cell15 = New Button()
+        cell14 = New Button()
+        cell13 = New Button()
+        cell12 = New Button()
+        cell23 = New Button()
+        cell22 = New Button()
+        cell21 = New Button()
+        cell20 = New Button()
+        cell19 = New Button()
+        cell18 = New Button()
+        cell29 = New Button()
+        cell28 = New Button()
+        cell27 = New Button()
+        cell26 = New Button()
+        cell25 = New Button()
+        cell24 = New Button()
+        cell35 = New Button()
+        cell34 = New Button()
+        cell33 = New Button()
+        cell32 = New Button()
+        cell31 = New Button()
+        cell30 = New Button()
+        startBtn = New Button()
+        restartBtn = New Button()
+        SuspendLayout()
+        ' 
+        ' cell00
+        ' 
+        cell00.Font = New Font("Microsoft Sans Serif", 8F)
+        cell00.Location = New Point(12, 12)
+        cell00.Name = "cell00"
+        cell00.Size = New Size(50, 50)
+        cell00.TabIndex = 0
+        cell00.UseVisualStyleBackColor = True
+        ' 
+        ' cell01
+        ' 
+        cell01.Font = New Font("Microsoft Sans Serif", 8F)
+        cell01.Location = New Point(68, 12)
+        cell01.Name = "cell01"
+        cell01.Size = New Size(50, 50)
+        cell01.TabIndex = 1
+        cell01.UseVisualStyleBackColor = True
+        ' 
+        ' cell02
+        ' 
+        cell02.Font = New Font("Microsoft Sans Serif", 8F)
+        cell02.Location = New Point(124, 12)
+        cell02.Name = "cell02"
+        cell02.Size = New Size(50, 50)
+        cell02.TabIndex = 2
+        cell02.UseVisualStyleBackColor = True
+        ' 
+        ' cell03
+        ' 
+        cell03.Font = New Font("Microsoft Sans Serif", 8F)
+        cell03.Location = New Point(180, 12)
+        cell03.Name = "cell03"
+        cell03.Size = New Size(50, 50)
+        cell03.TabIndex = 3
+        cell03.UseVisualStyleBackColor = True
+        ' 
+        ' cell04
+        ' 
+        cell04.Font = New Font("Microsoft Sans Serif", 8F)
+        cell04.Location = New Point(236, 12)
+        cell04.Name = "cell04"
+        cell04.Size = New Size(50, 50)
+        cell04.TabIndex = 4
+        cell04.UseVisualStyleBackColor = True
+        ' 
+        ' cell05
+        ' 
+        cell05.Font = New Font("Microsoft Sans Serif", 8F)
+        cell05.Location = New Point(292, 12)
+        cell05.Name = "cell05"
+        cell05.Size = New Size(50, 50)
+        cell05.TabIndex = 5
+        cell05.UseVisualStyleBackColor = True
+        ' 
+        ' cell11
+        ' 
+        cell11.Font = New Font("Microsoft Sans Serif", 8F)
+        cell11.Location = New Point(292, 68)
+        cell11.Name = "cell11"
+        cell11.Size = New Size(50, 50)
+        cell11.TabIndex = 11
+        cell11.UseVisualStyleBackColor = True
+        ' 
+        ' cell10
+        ' 
+        cell10.Font = New Font("Microsoft Sans Serif", 8F)
+        cell10.Location = New Point(236, 68)
+        cell10.Name = "cell10"
+        cell10.Size = New Size(50, 50)
+        cell10.TabIndex = 10
+        cell10.UseVisualStyleBackColor = True
+        ' 
+        ' cell09
+        ' 
+        cell09.Font = New Font("Microsoft Sans Serif", 8F)
+        cell09.Location = New Point(180, 68)
+        cell09.Name = "cell09"
+        cell09.Size = New Size(50, 50)
+        cell09.TabIndex = 9
+        cell09.UseVisualStyleBackColor = True
+        ' 
+        ' cell08
+        ' 
+        cell08.Font = New Font("Microsoft Sans Serif", 8F)
+        cell08.Location = New Point(124, 68)
+        cell08.Name = "cell08"
+        cell08.Size = New Size(50, 50)
+        cell08.TabIndex = 8
+        cell08.UseVisualStyleBackColor = True
+        ' 
+        ' cell07
+        ' 
+        cell07.Font = New Font("Microsoft Sans Serif", 8F)
+        cell07.Location = New Point(68, 68)
+        cell07.Name = "cell07"
+        cell07.Size = New Size(50, 50)
+        cell07.TabIndex = 7
+        cell07.UseVisualStyleBackColor = True
+        ' 
+        ' cell06
+        ' 
+        cell06.Font = New Font("Microsoft Sans Serif", 8F)
+        cell06.Location = New Point(12, 68)
+        cell06.Name = "cell06"
+        cell06.Size = New Size(50, 50)
+        cell06.TabIndex = 6
+        cell06.UseVisualStyleBackColor = True
+        ' 
+        ' cell17
+        ' 
+        cell17.Font = New Font("Microsoft Sans Serif", 8F)
+        cell17.Location = New Point(292, 124)
+        cell17.Name = "cell17"
+        cell17.Size = New Size(50, 50)
+        cell17.TabIndex = 17
+        cell17.UseVisualStyleBackColor = True
+        ' 
+        ' cell16
+        ' 
+        cell16.Font = New Font("Microsoft Sans Serif", 8F)
+        cell16.Location = New Point(236, 124)
+        cell16.Name = "cell16"
+        cell16.Size = New Size(50, 50)
+        cell16.TabIndex = 16
+        cell16.UseVisualStyleBackColor = True
+        ' 
+        ' cell15
+        ' 
+        cell15.Font = New Font("Microsoft Sans Serif", 8F)
+        cell15.Location = New Point(180, 124)
+        cell15.Name = "cell15"
+        cell15.Size = New Size(50, 50)
+        cell15.TabIndex = 15
+        cell15.UseVisualStyleBackColor = True
+        ' 
+        ' cell14
+        ' 
+        cell14.Font = New Font("Microsoft Sans Serif", 8F)
+        cell14.Location = New Point(124, 124)
+        cell14.Name = "cell14"
+        cell14.Size = New Size(50, 50)
+        cell14.TabIndex = 14
+        cell14.UseVisualStyleBackColor = True
+        ' 
+        ' cell13
+        ' 
+        cell13.Font = New Font("Microsoft Sans Serif", 8F)
+        cell13.Location = New Point(68, 124)
+        cell13.Name = "cell13"
+        cell13.Size = New Size(50, 50)
+        cell13.TabIndex = 13
+        cell13.UseVisualStyleBackColor = True
+        ' 
+        ' cell12
+        ' 
+        cell12.Font = New Font("Microsoft Sans Serif", 8F)
+        cell12.Location = New Point(12, 124)
+        cell12.Name = "cell12"
+        cell12.Size = New Size(50, 50)
+        cell12.TabIndex = 12
+        cell12.UseVisualStyleBackColor = True
+        ' 
+        ' cell23
+        ' 
+        cell23.Font = New Font("Microsoft Sans Serif", 8F)
+        cell23.Location = New Point(292, 180)
+        cell23.Name = "cell23"
+        cell23.Size = New Size(50, 50)
+        cell23.TabIndex = 23
+        cell23.UseVisualStyleBackColor = True
+        ' 
+        ' cell22
+        ' 
+        cell22.Font = New Font("Microsoft Sans Serif", 8F)
+        cell22.Location = New Point(236, 180)
+        cell22.Name = "cell22"
+        cell22.Size = New Size(50, 50)
+        cell22.TabIndex = 22
+        cell22.UseVisualStyleBackColor = True
+        ' 
+        ' cell21
+        ' 
+        cell21.Font = New Font("Microsoft Sans Serif", 8F)
+        cell21.Location = New Point(180, 180)
+        cell21.Name = "cell21"
+        cell21.Size = New Size(50, 50)
+        cell21.TabIndex = 21
+        cell21.UseVisualStyleBackColor = True
+        ' 
+        ' cell20
+        ' 
+        cell20.Font = New Font("Microsoft Sans Serif", 8F)
+        cell20.Location = New Point(124, 180)
+        cell20.Name = "cell20"
+        cell20.Size = New Size(50, 50)
+        cell20.TabIndex = 20
+        cell20.UseVisualStyleBackColor = True
+        ' 
+        ' cell19
+        ' 
+        cell19.Font = New Font("Microsoft Sans Serif", 8F)
+        cell19.Location = New Point(68, 180)
+        cell19.Name = "cell19"
+        cell19.Size = New Size(50, 50)
+        cell19.TabIndex = 19
+        cell19.UseVisualStyleBackColor = True
+        ' 
+        ' cell18
+        ' 
+        cell18.Font = New Font("Microsoft Sans Serif", 8F)
+        cell18.Location = New Point(12, 180)
+        cell18.Name = "cell18"
+        cell18.Size = New Size(50, 50)
+        cell18.TabIndex = 18
+        cell18.UseVisualStyleBackColor = True
+        ' 
+        ' cell29
+        ' 
+        cell29.Font = New Font("Microsoft Sans Serif", 8F)
+        cell29.Location = New Point(292, 236)
+        cell29.Name = "cell29"
+        cell29.Size = New Size(50, 50)
+        cell29.TabIndex = 29
+        cell29.UseVisualStyleBackColor = True
+        ' 
+        ' cell28
+        ' 
+        cell28.Font = New Font("Microsoft Sans Serif", 8F)
+        cell28.Location = New Point(236, 236)
+        cell28.Name = "cell28"
+        cell28.Size = New Size(50, 50)
+        cell28.TabIndex = 28
+        cell28.UseVisualStyleBackColor = True
+        ' 
+        ' cell27
+        ' 
+        cell27.Font = New Font("Microsoft Sans Serif", 8F)
+        cell27.Location = New Point(180, 236)
+        cell27.Name = "cell27"
+        cell27.Size = New Size(50, 50)
+        cell27.TabIndex = 27
+        cell27.UseVisualStyleBackColor = True
+        ' 
+        ' cell26
+        ' 
+        cell26.Font = New Font("Microsoft Sans Serif", 8F)
+        cell26.Location = New Point(124, 236)
+        cell26.Name = "cell26"
+        cell26.Size = New Size(50, 50)
+        cell26.TabIndex = 26
+        cell26.UseVisualStyleBackColor = True
+        ' 
+        ' cell25
+        ' 
+        cell25.Font = New Font("Microsoft Sans Serif", 8F)
+        cell25.Location = New Point(68, 236)
+        cell25.Name = "cell25"
+        cell25.Size = New Size(50, 50)
+        cell25.TabIndex = 25
+        cell25.UseVisualStyleBackColor = True
+        ' 
+        ' cell24
+        ' 
+        cell24.Font = New Font("Microsoft Sans Serif", 8F)
+        cell24.Location = New Point(12, 236)
+        cell24.Name = "cell24"
+        cell24.Size = New Size(50, 50)
+        cell24.TabIndex = 24
+        cell24.UseVisualStyleBackColor = True
+        ' 
+        ' cell35
+        ' 
+        cell35.Font = New Font("Microsoft Sans Serif", 8F)
+        cell35.Location = New Point(292, 292)
+        cell35.Name = "cell35"
+        cell35.Size = New Size(50, 50)
+        cell35.TabIndex = 35
+        cell35.UseVisualStyleBackColor = True
+        ' 
+        ' cell34
+        ' 
+        cell34.Font = New Font("Microsoft Sans Serif", 8F)
+        cell34.Location = New Point(236, 292)
+        cell34.Name = "cell34"
+        cell34.Size = New Size(50, 50)
+        cell34.TabIndex = 34
+        cell34.UseVisualStyleBackColor = True
+        ' 
+        ' cell33
+        ' 
+        cell33.Font = New Font("Microsoft Sans Serif", 8F)
+        cell33.Location = New Point(180, 292)
+        cell33.Name = "cell33"
+        cell33.Size = New Size(50, 50)
+        cell33.TabIndex = 33
+        cell33.UseVisualStyleBackColor = True
+        ' 
+        ' cell32
+        ' 
+        cell32.Font = New Font("Microsoft Sans Serif", 8F)
+        cell32.Location = New Point(124, 292)
+        cell32.Name = "cell32"
+        cell32.Size = New Size(50, 50)
+        cell32.TabIndex = 32
+        cell32.UseVisualStyleBackColor = True
+        ' 
+        ' cell31
+        ' 
+        cell31.Font = New Font("Microsoft Sans Serif", 8F)
+        cell31.Location = New Point(68, 292)
+        cell31.Name = "cell31"
+        cell31.Size = New Size(50, 50)
+        cell31.TabIndex = 31
+        cell31.UseVisualStyleBackColor = True
+        ' 
+        ' cell30
+        ' 
+        cell30.Font = New Font("Microsoft Sans Serif", 8F)
+        cell30.Location = New Point(12, 292)
+        cell30.Name = "cell30"
+        cell30.Size = New Size(50, 50)
+        cell30.TabIndex = 30
+        cell30.UseVisualStyleBackColor = True
+        ' 
+        ' startBtn
+        ' 
+        startBtn.Location = New Point(362, 94)
+        startBtn.Name = "startBtn"
+        startBtn.Size = New Size(80, 80)
+        startBtn.TabIndex = 36
+        startBtn.Text = "Start"
+        startBtn.UseVisualStyleBackColor = True
+        ' 
+        ' restartBtn
+        ' 
+        restartBtn.Location = New Point(362, 180)
+        restartBtn.Name = "restartBtn"
+        restartBtn.Size = New Size(80, 80)
+        restartBtn.TabIndex = 37
+        restartBtn.Text = "Restart"
+        restartBtn.UseVisualStyleBackColor = True
+        ' 
+        ' KnightsTour
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(469, 357)
+        Controls.Add(restartBtn)
+        Controls.Add(startBtn)
+        Controls.Add(cell35)
+        Controls.Add(cell34)
+        Controls.Add(cell33)
+        Controls.Add(cell32)
+        Controls.Add(cell31)
+        Controls.Add(cell30)
+        Controls.Add(cell29)
+        Controls.Add(cell28)
+        Controls.Add(cell27)
+        Controls.Add(cell26)
+        Controls.Add(cell25)
+        Controls.Add(cell24)
+        Controls.Add(cell23)
+        Controls.Add(cell22)
+        Controls.Add(cell21)
+        Controls.Add(cell20)
+        Controls.Add(cell19)
+        Controls.Add(cell18)
+        Controls.Add(cell17)
+        Controls.Add(cell16)
+        Controls.Add(cell15)
+        Controls.Add(cell14)
+        Controls.Add(cell13)
+        Controls.Add(cell12)
+        Controls.Add(cell11)
+        Controls.Add(cell10)
+        Controls.Add(cell09)
+        Controls.Add(cell08)
+        Controls.Add(cell07)
+        Controls.Add(cell06)
+        Controls.Add(cell05)
+        Controls.Add(cell04)
+        Controls.Add(cell03)
+        Controls.Add(cell02)
+        Controls.Add(cell01)
+        Controls.Add(cell00)
+        Name = "KnightsTour"
+        Text = "Knight's Tour"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents cell00 As Button
+    Friend WithEvents cell01 As Button
+    Friend WithEvents cell02 As Button
+    Friend WithEvents cell03 As Button
+    Friend WithEvents cell04 As Button
+    Friend WithEvents cell05 As Button
+    Friend WithEvents cell11 As Button
+    Friend WithEvents cell10 As Button
+    Friend WithEvents cell09 As Button
+    Friend WithEvents cell08 As Button
+    Friend WithEvents cell07 As Button
+    Friend WithEvents cell06 As Button
+    Friend WithEvents cell17 As Button
+    Friend WithEvents cell16 As Button
+    Friend WithEvents cell15 As Button
+    Friend WithEvents cell14 As Button
+    Friend WithEvents cell13 As Button
+    Friend WithEvents cell12 As Button
+    Friend WithEvents cell23 As Button
+    Friend WithEvents cell22 As Button
+    Friend WithEvents cell21 As Button
+    Friend WithEvents cell20 As Button
+    Friend WithEvents cell19 As Button
+    Friend WithEvents cell18 As Button
+    Friend WithEvents cell29 As Button
+    Friend WithEvents cell28 As Button
+    Friend WithEvents cell27 As Button
+    Friend WithEvents cell26 As Button
+    Friend WithEvents cell25 As Button
+    Friend WithEvents cell24 As Button
+    Friend WithEvents cell35 As Button
+    Friend WithEvents cell34 As Button
+    Friend WithEvents cell33 As Button
+    Friend WithEvents cell32 As Button
+    Friend WithEvents cell31 As Button
+    Friend WithEvents cell30 As Button
+    Friend WithEvents startBtn As Button
+    Friend WithEvents restartBtn As Button
+
+End Class
---
+++ Knight's Tour/Knight's Tour/Form1.vb
@@ -0,0 +1,99 @@
+﻿Public Class KnightsTour
+    Dim n As Integer = 6, steps, prevRow, prevCol As Integer
+    Dim cells(n, n) As Button
+    Dim board(n, n) As Integer
+
+    Private Sub KnightsTour_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For i = 0 To n ^ 2 - 1
+            Dim r As Integer = i \ n, c As Integer = i Mod n
+            If i < 10 Then
+                cells(r, c) = DirectCast(Controls.Find("cell0" & i, False).First, Button)
+            Else
+                cells(r, c) = DirectCast(Controls.Find("cell" & i, False).First, Button)
+            End If
+            cells(r, c).Enabled = False
+            cells(r, c).Text = ""
+            board(r, c) = 0
+        Next
+        steps = 0
+        prevRow = -1
+        prevCol = -1
+        startBtn.Enabled = True
+        restartBtn.Enabled = False
+    End Sub
+
+    Private Function IsValidMove(prevRow As Integer, prevCol As Integer, currRow As Integer, currCol As Integer)
+        If prevRow = -1 And prevCol = -1 Then
+            Return True
+        End If
+        Return (Math.Abs(currRow - prevRow) = 1 And Math.Abs(currCol - prevCol) = 2) Or (Math.Abs(currRow - prevRow) = 2 And Math.Abs(currCol - prevCol) = 1)
+    End Function
+
+    Private Function NoValidMoves(n As Integer, board(,) As Integer, row As Integer, col As Integer)
+        Dim dy() As Integer = {-2, -1, 1, 2, 2, 1, -1, -2}
+        Dim dx() As Integer = {1, 2, 2, 1, -1, -2, -2, -1}
+        For i = 0 To 7
+            If row + dy(i) >= 0 And row + dy(i) <= n - 1 And col + dx(i) >= 0 And col + dx(i) <= n - 1 Then
+                If board(row + dy(i), col + dx(i)) = 0 Then
+                    Return False
+                End If
+            End If
+        Next
+        Return True
+    End Function
+
+    Private Sub GameOver()
+        For i = 0 To n - 1
+            For j = 0 To n - 1
+                cells(i, j).Enabled = False
+            Next
+        Next
+        restartBtn.Enabled = True
+    End Sub
+
+    Private Sub button_Click(sender As Object, e As EventArgs) Handles cell35.Click, cell34.Click, cell33.Click, cell32.Click, cell31.Click, cell30.Click, cell29.Click, cell28.Click, cell27.Click, cell26.Click, cell25.Click, cell24.Click, cell23.Click, cell22.Click, cell21.Click, cell20.Click, cell19.Click, cell18.Click, cell17.Click, cell16.Click, cell15.Click, cell14.Click, cell13.Click, cell12.Click, cell11.Click, cell10.Click, cell09.Click, cell08.Click, cell07.Click, cell06.Click, cell05.Click, cell04.Click, cell03.Click, cell02.Click, cell01.Click, cell00.Click
+        Dim cellName As String = CType(sender, Button).Name
+        Dim cellNum As Integer = CType(cellName.Substring(4), Integer)
+        Dim row As Integer = cellNum \ n, col As Integer = cellNum Mod n
+        If IsValidMove(prevRow, prevCol, row, col) Then
+            steps += 1
+            cells(row, col).Text = steps.ToString()
+            cells(row, col).Enabled = False
+            board(row, col) = steps
+            prevRow = row
+            prevCol = col
+            If steps = n ^ 2 Then
+                GameOver()
+                MessageBox.Show("Solved!")
+            ElseIf NoValidMoves(n, board, row, col) Then
+                GameOver()
+                MessageBox.Show("Game Over!")
+            End If
+        Else
+            MessageBox.Show("Invalid move")
+        End If
+    End Sub
+
+    Private Sub startBtn_Click(sender As Object, e As EventArgs) Handles startBtn.Click
+        For i = 0 To n - 1
+            For j = 0 To n - 1
+                cells(i, j).Enabled = True
+            Next
+        Next
+        startBtn.Enabled = False
+    End Sub
+
+    Private Sub restartBtn_Click(sender As Object, e As EventArgs) Handles restartBtn.Click
+        For i = 0 To n - 1
+            For j = 0 To n - 1
+                cells(i, j).Text = ""
+                board(i, j) = 0
+            Next
+        Next
+        steps = 0
+        prevRow = -1
+        prevCol = -1
+        startBtn.Enabled = True
+        restartBtn.Enabled = False
+    End Sub
+End Class
